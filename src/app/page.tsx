098c47c0cf6f24b2aa648a0cda0d7d2668452254
Plentk <149661762+Plentk@users.noreply.github.com>
2024-12-10 14:28:35 +0800

    Version 0.01
    Added 2 pages
    Removed default stuff
    Imported card, flip-words, navigation-menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,152 @@
 import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import React from "react";
+import { FlipWords } from "@/components/ui/flip-words";
+
 
 export default function Home() {
+  const words = [" start"," continue"," do"," consider"," think about"," consider taking"," buy"];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly..</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    
+    <div>
+      <div className="h-[20rem] flex justify-center items-center px-4">
+      <div className="text-6xl mx-auto font-normal text-neutral-800 dark:text-neutral-800">
+        Don't 
+        <FlipWords words={words} />
+        drugs
+      </div>
+    </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Pls dont do drugs
+          </CardTitle>
+          <CardDescription>
+            <p>drugs are bad, just bad</p>
+            drugs are bad, just bad
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Yes they are bad why you still thinking
+        </CardContent>
+        <CardFooter>
+          drugs = bad
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Pls pls dont do drugs
+          </CardTitle>
+          <CardDescription>
+            <p>drugs are very bad, very bad</p>
+            drugs are very bad, just bad
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Yes they ARE bad why you still thinking of taking drugs
+        </CardContent>
+        <CardFooter>
+          drugs {'>'}= very bad
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Alcohol
+          </CardTitle>
+          <CardDescription>
+            Alcohol (Ethanol) is C2H5OH
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Alcohol makes one high. High people does weird stuff
+          (Only ethanol can be consumed. Pls do not consume other alcohols)
+        </CardContent>
+        <CardFooter>
+          not drugs (i think?)
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Coffee
+          </CardTitle>
+          <CardDescription>
+            Coffee contains caffeine
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Coffee increases energy, pushing back sleeping time. May get addicted
+        </CardContent>
+        <CardFooter>
+          causes problems
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Drrrruuuugggssss
+          </CardTitle>
+          <CardDescription>
+            high
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          oll34
+        </CardContent>
+        <CardFooter>
+          <CardTitle>
+            Drugs
+          </CardTitle>
+          <CardDescription>
+            help i am being held hostage pls save me 
+          </CardDescription>
+        </CardFooter>
+      </Card>
+      <Card className="bg-zinc-500">
+        hi
+        <CardHeader>
+          hi
+          <CardTitle>
+            hibbjjb
+          </CardTitle>
+          <CardDescription>
+            hi
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          ghvhhggj
+      
+        </CardContent>
+        <CardFooter>
+          iijvhv
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+          ☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️
+          </CardTitle>
+          <CardDescription>
+            
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+        Thhhis... hic th-th-thishh... hic whaaat... hic whaaa... hic drugshhhh doooo... hic d-don't... hic d-don't... hic noooo... hic noooo... yawn... hic snore... hic... zzZzz...
+      
+        </CardContent>
+        <CardFooter>
+          iijvhvekekepp3rp3le;dl;lsdwddewodjjdwojdjwd
+        </CardFooter>
+      </Card>
     </div>
   );
 }
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,7 @@
+export default function main() {
+    return (
+        <div>
+            Quiz me, not i quiz u
+        </div>
+    )
+}
